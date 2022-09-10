790ea33c16ba9d9e3261e103aa5585821aa2e128
GuDeBin <gudebin@outlook.com>
2022-09-10 22:58:47 +0800

    Site updated: 2022-09-10 22:58:47

--- src/components/Flipper.tsx
+++ src/components/Flipper.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useMemo, useRef } from "react";
 import { FlipContext, FlipItemType, IFlipContext } from "./FlipContext";
 
 export interface FlipperProps {
   flipKey: unknown;
+  animateOptions?: KeyframeAnimationOptions;
+  children: React.ReactNode;
+}
+
+export default function Flipper({
+  flipKey,
+  animateOptions,
+  children,
+}: FlipperProps) {
+  const lastRectRef = useRef<Map<number, FlipItemType>>(new Map());
+  const uniqueIdRef = useRef(0);
+
+  const fnRef = useRef<IFlipContext>({
+    add(item) {
+      lastRectRef.current.set(item.flipId, item);
+    },
+    remove(flipId) {
+      lastRectRef.current.delete(flipId);
+    },
+    nextId() {
+      return (uniqueIdRef.current += 1);
+    },
+  });
+
+  useMemo(() => {
+    lastRectRef.current.forEach((item) => {
+      item.rect = item.node.getBoundingClientRect();
+    });
+  }, [flipKey]);
 }
